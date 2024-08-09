0fa39f51844bbdf707585f0fe6ef8bbbf2ec4c0d
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-08-09 20:43:49 +0500

    resvole error

--- app/Provider/Provider.tsx
+++ app/Provider/Provider.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import React from "react";
 import { SessionProvider } from "next-auth/react";
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return <Provider store={store}>{children}</Provider>;
 };
