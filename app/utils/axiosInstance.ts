823d07530a313cbeac54fe346c212740e0e83f1c
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-08-11 15:12:36 +0500

    cors problem

--- app/utils/axiosInstance.ts
+++ app/utils/axiosInstance.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-export const BASE_URL =
-  "https://doctor-appointment-dashboard-ahmad-422z5jreq.vercel.app";
+export const BASE_URL = "https://doctor-appointment-dashboard-ahmad.vercel.app";
 export const AxiosInstance = axios.create({
-  baseURL: "https://doctor-appointment-dashboard-ahmad-3yr77d2pa.vercel.app",
+  baseURL: "https://doctor-appointment-dashboard-ahmad.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
