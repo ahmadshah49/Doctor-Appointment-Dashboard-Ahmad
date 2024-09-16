40de925ed0c0ec623a51471cba6c085f4f1ad56a
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-09-16 15:55:35 +0500

    feat: add base url

--- app/utils/axiosInstance.ts
+++ app/utils/axiosInstance.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "https://doctor-appointment-dashboard-ahmad.vercel.app";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://doctor-appointment-dashboard-ahmad.vercel.app";
