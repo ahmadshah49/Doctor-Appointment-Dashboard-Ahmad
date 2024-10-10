0925c810237b9b22583f501c4382fa6148d7afb3
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-10-10 12:45:24 +0500

    Fix: add Pie Chart

--- app/components/charts/totalPatients/TotalPatientsChart.tsx
+++ app/components/charts/totalPatients/TotalPatientsChart.tsx
@@ -65,18 +65,15 @@ const TotalPatientsChart: React.FC = () => {
   return (
     <div className="relative xl:max-h-[112px] xl:max-w-[164px] w-full h-full">
       <span className="absolute inset-0 flex flex-col items-center justify-center">
-        {onlineConsultations?.length && (
-          <p>
-            <span>{onlineConsultations?.length} </span>
-            <span className="text-[#2F80ED] ">Online</span>
-          </p>
-        )}
-        {offlineConsultations?.length && (
-          <p>
-            <span>{offlineConsultations?.length} </span>
-            <span className="text-[#EB5757] ">Offline</span>
-          </p>
-        )}
+        <p>
+          <span>{onlineConsultations?.length} </span>
+          <span className="text-[#2F80ED] ">Online</span>
+        </p>
+
+        <p>
+          <span>{offlineConsultations?.length} </span>
+          <span className="text-[#EB5757] ">Offline</span>
+        </p>
       </span>
 
       <Doughnut
